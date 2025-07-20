22e2c3df5e7e713be040c4dd7e1629d3ff2cbaa1
AhmaduLo <ahmadubambagayelo@gmail.com>
2025-07-20 19:54:49 +0200

    Initial commit

---
+++ src/main/java/com/example/coindecoback/CoinDecoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.coindecoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDecoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinDecoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coindecoback/CoinDecoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coindecoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDecoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
